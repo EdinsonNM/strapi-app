0c421f6ade3d3b3b9ef37f6ee91995dfbfdf1709
hencarpa <hencarpa@gmail.com>
2022-12-16 11:35:30 -0500

    final programas

---
+++ src/api/diseno/controllers/diseno.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * diseno controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::diseno.diseno');
---
+++ src/api/diseno/services/diseno.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * diseno service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::diseno.diseno');
