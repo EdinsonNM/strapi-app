7ee7f95e68163e27916b7fb71d0ead8f2151ee3b
Edinson Nuñez More <nmedinson@gmail.com>
2022-12-16 01:45:04 -0500

    first commit

---
+++ src/api/egresado/controllers/egresado.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * egresado controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::egresado.egresado');
